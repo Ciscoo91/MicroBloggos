8c099b3cd51faeaed64a90250e97595a368f9311
Ciscoo91 <franciscomambo91@gmail.com>
2023-03-12 23:31:21 +0100

    updated PrivateRoute component

--- client/src/components/auth/PrivateRoute.js
+++ client/src/components/auth/PrivateRoute.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../../context/AuthContext';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ children}) => {
 
     const { user } = useContext(AuthContext);
 
-    return (
-        <Route {...rest} render={(routeProps) => (
-            user.isLoggedIn ? <Component {...routeProps} /> : <Navigate to="/" />
-        )} />
-    );
+    if(!user.isLoggedIn){
+        return <Navigate to="/" />
+    }
+
+    return children
+    
 }
 
 export default PrivateRoute;
